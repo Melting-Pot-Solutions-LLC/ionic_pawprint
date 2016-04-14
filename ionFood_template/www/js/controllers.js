49203ae659e208e06a21ffdf00381ae67727b8b4
laciecochran <laciecochran@yahoo.com>
2016-04-14 16:51:14 -0400

    color coded map markers that are smaller than originals

--- ionFood_template/www/js/controllers.js
+++ ionFood_template/www/js/controllers.js
@@ -72,58 +72,67 @@ angular.module('starter.controllers', ['firebase'])
 })
 
 .controller('Location', function($scope, $ionicLoading, $compile, Places) {
+  var mapIcon = {
+    path: '',
+    fillColor: '',
+    fillOpacity: 0.8,
+    scale: 1,
+    strokeColor: '',
+    strokeWeight: 14
+  };
+
   /**
- * The CenterControl adds a control to the map that recenters the map on
- * the user.
- * This constructor takes the control DIV as an argument.
- * @constructor
- */
-function CenterControl(controlDiv, map) {
+   * The CenterControl adds a control to the map that recenters the map on
+   * the user.
+   * This constructor takes the control DIV as an argument.
+   * @constructor
+   */
+  function CenterControl(controlDiv, map) {
 
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.marginTop = '10px';
-  controlUI.style.marginRight = '10px';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to recenter the map';
-  controlDiv.appendChild(controlUI);
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginTop = '10px';
+    controlUI.style.marginRight = '10px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to recenter the map';
+    controlDiv.appendChild(controlUI);
 
-  // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '12px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Center Map';
-  controlUI.appendChild(controlText);
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '12px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Center Map';
+    controlUI.appendChild(controlText);
 
-  // Setup the click event listeners: simply set the map to Chicago.
-  controlUI.addEventListener('click', function() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        myLatlng = pos;
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, map.getCenter());
-    }
-  });
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          myLatlng = pos;
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, map.getCenter());
+      }
+    });
 
-}
+  }
 
   function initialize() {
     var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
@@ -212,6 +221,19 @@ function CenterControl(controlDiv, map) {
         title: $scope.places_in_database[i].name
       });
 
+      if($scope.places_in_database[i].type == "RB"){
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+      }
+      else if($scope.places_in_database[i].type == "Vet"){
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+      }
+      else if($scope.places_in_database[i].type == "Park"){
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      }
+      else {
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/black-dot.png');
+      }
+
       $scope.markers.push(marker);
       $scope.places_to_show.push($scope.places_in_database[i]);
 
@@ -237,7 +259,8 @@ function CenterControl(controlDiv, map) {
         var marker = new google.maps.Marker({
           position: pos,
           map: $scope.map,
-          title: $scope.places_in_database[i].name
+          title: $scope.places_in_database[i].name,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
         });
 
         $scope.places_to_show.push($scope.places_in_database[i]);
@@ -265,7 +288,8 @@ function CenterControl(controlDiv, map) {
         var marker = new google.maps.Marker({
           position: pos,
           map: $scope.map,
-          title: $scope.places_in_database[i].name
+          title: $scope.places_in_database[i].name,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
         });
 
         $scope.places_to_show.push($scope.places_in_database[i]);
@@ -290,7 +314,8 @@ function CenterControl(controlDiv, map) {
         var marker = new google.maps.Marker({
           position: pos,
           map: $scope.map,
-          title: $scope.places_in_database[i].name
+          title: $scope.places_in_database[i].name,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
         });
 
         $scope.places_to_show.push($scope.places_in_database[i]);
