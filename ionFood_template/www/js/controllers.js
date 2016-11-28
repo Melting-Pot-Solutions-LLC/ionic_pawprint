f5f45d25f9f9ec19d1ae77b358e25e572ed9be6b
steveEECSrubin <konstantinrubin@engineer.com>
2016-11-28 16:59:52 -0500

    .

--- ionFood_template/www/js/controllers.js
+++ ionFood_template/www/js/controllers.js
@@ -42,7 +42,7 @@ angular.module('starter.controllers', ['firebase'])
   console.log("in the main menu");
 })
 
-.controller('LoginCtrl', function($scope, $firebaseAuth, $state, $rootScope, $ionicPopup) {
+.controller('LoginCtrl', function($scope, $firebaseAuth, $state, $rootScope, $ionicPopup, $ionicLoading, $q) {
 
   function httpGetAsync(theUrl, callback)
   {
@@ -137,6 +137,113 @@ angular.module('starter.controllers', ['firebase'])
   //     }
   //   });
   // }
+
+
+
+
+
+
+  //-- NATIVE LOGIN
+    // This is the success callback from the login method
+  var fbLoginSuccess = function(response) {
+    if (!response.authResponse){
+      fbLoginError("Cannot find the authResponse");
+      return;
+    }
+
+    var authResponse = response.authResponse;
+
+    getFacebookProfileInfo(authResponse)
+    .then(function(profileInfo) {
+      // For the purpose of this example I will store user data on local storage
+    //   UserService.setUser({
+    //     authResponse: authResponse,
+	// 			userID: profileInfo.id,
+	// 			name: profileInfo.name,
+	// 			email: profileInfo.email,
+    //     picture : "http://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
+    //   });
+      $ionicLoading.hide();
+      $state.go('app.home');
+    }, function(fail){
+      // Fail get profile info
+      console.log('profile info fail', fail);
+    });
+  };
+
+  // This is the fail callback from the login method
+  var fbLoginError = function(error){
+    console.log('fbLoginError', error);
+    $ionicLoading.hide();
+  };
+
+  // This method is to get the user profile info from the facebook api
+  var getFacebookProfileInfo = function (authResponse) {
+    var info = $q.defer();
+
+    facebookConnectPlugin.api('/me?fields=email,name&access_token=' + authResponse.accessToken, null,
+      function (response) {
+				console.log(response);
+        info.resolve(response);
+      },
+      function (response) {
+				console.log(response);
+        info.reject(response);
+      }
+    );
+    return info.promise;
+  };
+
+  //This method is executed when the user press the "Login with facebook" button
+  $scope.facebookSignIn = function() {
+    facebookConnectPlugin.getLoginStatus(function(success){
+      if(success.status === 'connected'){
+        // The user is logged in and has authenticated your app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed request, and the time the access token
+        // and signed request each expire
+        console.log('getLoginStatus', success.status);
+
+
+    		if(!user.userID){
+					getFacebookProfileInfo(success.authResponse)
+					.then(function(profileInfo) {
+						// For the purpose of this example I will store user data on local storage
+						// UserService.setUser({
+						// 	authResponse: success.authResponse,
+						// 	userID: profileInfo.id,
+						// 	name: profileInfo.name,
+						// 	email: profileInfo.email,
+						// 	picture : "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
+						// });
+
+						//$state.go('app.home');
+					}, function(fail){
+						// Fail get profile info
+						console.log('profile info fail', fail);
+					});
+				}else{
+					$state.go('app.home');
+                    console.log("successfully logged in!");
+				}
+      } else {
+        // If (success.status === 'not_authorized') the user is logged in to Facebook,
+				// but has not authenticated your app
+        // Else the person is not logged into Facebook,
+				// so we're not sure if they are logged into this app or not.
+
+				console.log('getLoginStatus', success.status);
+
+				$ionicLoading.show({
+          template: 'Logging in...'
+        });
+
+				// Ask the permissions you need. You can learn more about
+				// FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+        facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
+      }
+    });
+  };
+  //--
 })
 
 .controller('DetailController', function($scope, $ionicNavBarDelegate, $stateParams, $rootScope, $state) {
@@ -319,6 +426,24 @@ angular.module('starter.controllers', ['firebase'])
 
         $scope.places_to_show.push($scope.places_in_database[i]);
         $scope.markers.push(marker);
+
+        //opens up an infowindow and delete the previous one 
+        var content = $scope.places_in_database[i].name;
+        var infowindow = new google.maps.InfoWindow();
+        var prev_infowindow = false; 
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+        {
+          return function() 
+          {
+            if( prev_infowindow )
+            {
+              prev_infowindow.close();
+            }
+            prev_infowindow = infowindow;
+            infowindow.setContent(content);
+            infowindow.open(map5,marker);
+          };
+        })(marker,content,infowindow)); 
       }
     }
     console.log("displaying only RB");
@@ -418,6 +543,24 @@ angular.module('starter.controllers', ['firebase'])
         }
         $scope.places_to_show.push($scope.places_in_database[i]);
         $scope.markers.push(marker);
+
+        //opens up an infowindow and delete the previous one 
+        var content = $scope.places_in_database[i].name;
+        var infowindow = new google.maps.InfoWindow();
+        var prev_infowindow = false; 
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+        {
+          return function() 
+          {
+            if( prev_infowindow )
+            {
+              prev_infowindow.close();
+            }
+            prev_infowindow = infowindow;
+            infowindow.setContent(content);
+            infowindow.open(map6,marker);
+          };
+        })(marker,content,infowindow)); 
       }
     }
     console.log("displaying only V and P");
@@ -585,6 +728,25 @@ angular.module('starter.controllers', ['firebase'])
       $scope.markers.push(marker);
       $scope.places_to_show.push($scope.places_in_database[i]);
 
+
+      //opens up an infowindow and delete the previous one 
+      var content = $scope.places_in_database[i].name;
+      var infowindow = new google.maps.InfoWindow();
+      var prev_infowindow = false; 
+      google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+      {
+        return function() 
+        {
+          if( prev_infowindow )
+          {
+            prev_infowindow.close();
+          }
+          prev_infowindow = infowindow;
+          infowindow.setContent(content);
+          infowindow.open(map,marker);
+        };
+      })(marker,content,infowindow)); 
+
     }
 
     //added by Steve
@@ -612,6 +774,26 @@ angular.module('starter.controllers', ['firebase'])
 
         $scope.places_to_show.push($scope.places_in_database[i]);
         $scope.markers.push(marker);
+
+        //opens up an infowindow and delete the previous one 
+        var content = $scope.places_in_database[i].name;
+        var infowindow = new google.maps.InfoWindow();
+        var prev_infowindow = false; 
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+        {
+          return function() 
+          {
+            if( prev_infowindow )
+            {
+              prev_infowindow.close();
+            }
+            prev_infowindow = infowindow;
+            infowindow.setContent(content);
+            infowindow.open(map,marker);
+          };
+        })(marker,content,infowindow)); 
+
+
       }
     }
     console.log("displaying only RB");
@@ -638,6 +820,24 @@ angular.module('starter.controllers', ['firebase'])
 
         $scope.places_to_show.push($scope.places_in_database[i]);
         $scope.markers.push(marker);
+
+        //opens up an infowindow and delete the previous one 
+        var content = $scope.places_in_database[i].name;
+        var infowindow = new google.maps.InfoWindow();
+        var prev_infowindow = false; 
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+        {
+          return function() 
+          {
+            if( prev_infowindow )
+            {
+              prev_infowindow.close();
+            }
+            prev_infowindow = infowindow;
+            infowindow.setContent(content);
+            infowindow.open(map,marker);
+          };
+        })(marker,content,infowindow)); 
       }
     }
     console.log("displaying only vets");
@@ -664,6 +864,25 @@ angular.module('starter.controllers', ['firebase'])
 
         $scope.places_to_show.push($scope.places_in_database[i]);
         $scope.markers.push(marker);
+
+        //opens up an infowindow and delete the previous one 
+        var content = $scope.places_in_database[i].name;
+        var infowindow = new google.maps.InfoWindow();
+        var prev_infowindow = false; 
+        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow)
+        {
+          return function() 
+          {
+            if( prev_infowindow )
+            {
+              prev_infowindow.close();
+            }
+            prev_infowindow = infowindow;
+            infowindow.setContent(content);
+            infowindow.open(map,marker);
+          };
+        })(marker,content,infowindow)); 
+
       }
     }
     console.log("displaying only parks");
